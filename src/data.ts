e63713947b4c58f0f1d3f1b2b696855f0b63d644
Paradorn-248 <paradorn248@gmail.com>
2022-09-27 04:19:18 +0700

    wait for check

--- src/data.ts
+++ src/data.ts
@@ -23,7 +23,7 @@ export const technologies = [
   {
     section: "Frameworks:",
     details:
-      "ExpressJS, Fastapi, Docusaurus 2",
+      "ExpressJS, FastAPI, Docusaurus 2",
   },
   {
     section: "Tools:",
@@ -78,7 +78,7 @@ export const workExperiences = [
     url: "https://web.facebook.com/DAT.KUSRC",
     years: ["7", "8 May 2022"],
     details: [
-      "I participated in Digital Academy Thailand to learn AI and Machine Learning course using tensorflow and scikit-learn."
+      "I participated in Digital Academy Thailand to learn AI and Machine Learning course using scikit-learn and tensorflow."
     ],
   },
 
@@ -95,32 +95,33 @@ export const projects = [
   {
     name: "Safe-Home",
     details: [
-      "This project is designed to keep homes safe from accidents such as floods, gas leaks, fires, earthquakes so that homeowners can deal with them in time.",
+      "This project is designed to keep homes safe from accidents such as floods, gas leaks, fires, earthquakes so that homeowners can deal with them in time. This project use FastAPI as backend, use html, React, CSS as frontend, use Arduino to control sensor.",
     ],
     url: "github.com/Exceed-Safe-Home"
   },
   {
     name: "Pet feeder with Arduino color sensor",
     details: [
-      "",
+      "This project was created so that people who own dogs or cats don't have to hire someone to take care of their pets when they're not around or some people have different kinds of animals, they have to feed differently. This machine will feed the pet only when the animal is within the operating range of the machine and wears a shirt of the specific color to the machine color sensor. This project use Arduion to control sensor.",
     ],
     url: "github.com/Paradorn-248/Pet_feeder-Project"
   },
   {
     name: "Predicting the Chance of Lung Disease using Patient Data and Identifying the Type of Lung Disease by CT scans",
     details: [
-      "",
+      "This project is about lung cancer. It was created to save people the cost of CT scans and help the doctor to diagnose the disease correctly. Initially, the Decision Tree Classifier model was used to predict from a patient's personal data whether they were likely to develop lung cancer. If the result comes out that there is a chance. We will use deep learning to help predict  type of lung cancer with CT scans picture. This project use scikit-learn and tensorflow",
     ],
     url: "github.com/Paradorn-248/MIKELab-Project"
   },
   {
     name: "Tetris Upgrade",
     details: [
-      "",
+      "This project is an upgraded tetris game. It has more difficult than normal tetris with more obstacle to play, screen will brighter so you can not see any block.you can use your hand cover light sensor to remove this obstacle.This project use Pygame and raspberry to connect game and controller.",
     ],
     url: "github.com/Paradorn-248/PracticumProject-TetrisGame"
   },
 ]
+
 export const references = [
   {
     name: "Dr. Monchai Sopitkamon",
