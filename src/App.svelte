5bdf3b40aff624724c9b3d2d70c37107ff7cb0ab
Paradorn-248 <paradorn248@gmail.com>
2022-09-27 18:28:53 +0700

    check sentence

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,9 @@
 
 <Tailwind />
 
-<header class="web-only text-center p-4 sm:p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white w-screen">
+<header
+  class="web-only text-center p-4 sm:p-6 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white w-screen"
+>
   <h1 class="text-4xl">Resume</h1>
   <h3>
     <button on:click={toggleMode} class="underline text-lg"
@@ -100,24 +102,23 @@
     <h2 class="text-2xl print:text-4xl uppercase text-left">Projects</h2>
     <hr />
 
-    <ul class="text-left list-disc pl-8">
-      {#each projects as project}
-        
-          <HideToggle />
-          {#if project.url == undefined}
-            <strong>{project.name}</strong>
-          {:else}
-            <a href="https://{project.url}" target="_blank" rel="noreferrer"
-              ><strong>{project.name}</strong></a
-            >
-          {/if}
-          <li>
+    {#each projects as project}
+      <HideToggle />
+      {#if project.url == undefined}
+        <strong>{project.name}</strong>
+      {:else}
+        <a href="https://{project.url}" target="_blank" rel="noreferrer"
+          ><strong>{project.name}</strong></a
+        >
+      {/if}
+      <ul class="text-left list-disc pl-8">
+        <li>
           {#each project.details as detail}
             <ul class="index">{detail}</ul>
           {/each}
-          </li>
-      {/each}
-    </ul>
+        </li>
+      </ul>
+    {/each}
   </section>
 
   <section>
